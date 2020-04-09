bab09258be020a5a6cf1b5c0745f6e234409d465
MaryHooker <mc020791@yahoo.com>
2020-04-09 11:53:34 -0500

    routes created & tested

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,15 @@
 let express = require('express');
 let router = express.Router();
 
+//READ IN CRUD
+router.get('/:id',(req,res)=>{
+    res.send(`Welcome back ${req.params.id}`);
+})
+
+//READ IN CRUD
+router.get('/',(req,res)=>{
+    res.send('Hello new user');
+})
 
 //export router
 module.exports = router;
